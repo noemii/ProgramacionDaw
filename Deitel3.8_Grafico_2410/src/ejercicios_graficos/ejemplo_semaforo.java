4142d3c73e30f622e0437c99b37ddc6a94db670f
noemii <nana.fad29@hotmail.com>
2013-12-12 18:03:02 +0100

    Ejemplo semaforo con movimiento

--- Deitel3.8_Grafico_2410/src/ejercicios_graficos/ejemplo_semaforo.java
+++ Deitel3.8_Grafico_2410/src/ejercicios_graficos/ejemplo_semaforo.java
@@ -11,40 +11,45 @@ public class ejemplo_semaforo {
 		
 		Canvas cuadro;
 		cuadro = Canvas.getInstance();
-	
+		int i=0;
 		
-		Rectangle rectangulo;
-		rectangulo = new Rectangle(0,0,50,150); //0,0 es para indicar donde empieza el rectangulo
-		rectangulo.setColor(Color.BLACK);
-		rectangulo.draw();
-		rectangulo.fill();
-		
-		Ellipse circulo;
-		circulo = new Ellipse(10, 10, 30, 30);
-		circulo.draw();
-		circulo.setColor(Color.RED);
-		circulo.fill();
-		
-		Thread.sleep(1000);
-		
-		Ellipse circulo2 = new Ellipse(10,60,30,30);
-		circulo2.draw();
-		circulo2.setColor(Color.YELLOW);
-		circulo2.fill();
-		
-		Thread.sleep(1000);
-		
-		Ellipse circulo3 = new Ellipse (10,110,30,30);
-		circulo3.draw();
-		circulo3.setColor(Color.GREEN);
-		circulo3.fill();
-		
-		
-		
-		
-		
-		
-		
+		do{
+			for(; i<10; i++){
+					Rectangle rectangulo;
+					rectangulo = new Rectangle(0,0,50,150); //0,0 es para indicar donde empieza el rectangulo
+					rectangulo.setColor(Color.BLACK);
+					rectangulo.draw();
+					rectangulo.fill();
+					
+					crearEllipse(10, 10, 30, 30, Color.RED);
+					Thread.sleep(1000);
+					crearEllipse(10, 10, 30, 30, Color.BLACK);
+					
+					crearEllipse(10,60,30,30, Color.YELLOW);
+					Thread.sleep(1000);
+					crearEllipse(10,60,30,30, Color.BLACK);
+					
+					crearEllipse(10,110,30,30, Color.GREEN);
+					Thread.sleep(1000);
+					crearEllipse(10,110,30,30, Color.BLACK);
+					
+					
+					//SI NO QUIERO MOVIMIENTO LE QUITO LOS CIRCULOS DE COLOR NEGRO Y EL THREAD SLEEP.
+					//TAMBIEN TENDRE QUE QUITAR EL FOR Y WHILE
+				
+			
+			}
+		}while(i<=10);
 	}
 
+	private static void crearEllipse(int i, int j, int k, int l, Color color) {
+		Ellipse circulo;
+		circulo = new Ellipse(i, j, k, l);
+		circulo.draw();
+		circulo.setColor(color);
+		circulo.fill();
+		
+	}
 }
+
+
